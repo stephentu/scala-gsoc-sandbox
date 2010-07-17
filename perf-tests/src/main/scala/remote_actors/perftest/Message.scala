280455a2e4a677e2dd583f1f0a81b2428194b827
Stephen Tu <stephen_tu@berkeley.edu>
2010-07-17 15:11:02 -0700

    fix node test

--- perf-tests/src/main/scala/remote_actors/perftest/Message.scala
+++ perf-tests/src/main/scala/remote_actors/perftest/Message.scala
@@ -4,3 +4,7 @@ package perftest
 case class Message(bytes: Array[Byte], timeCreated: Long) {
   def timeElasped = System.nanoTime - timeCreated
 }
+
+case class NodeMessage(bytes: Array[Byte], timeCreated: Long, machine: String, runId: Int, actorId: Int, msgId: Int, hasEchoed: Boolean) {
+  def timeElasped = System.nanoTime - timeCreated
+}
